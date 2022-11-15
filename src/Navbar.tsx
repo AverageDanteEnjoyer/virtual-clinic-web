f7c3be781e3d64df0d576534c2ea3d8b987c6b27
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2022-11-15 18:07:10 +0100

    link react-router library

---
+++ src/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
+
+const Navbar:React.FC=(props:any) => {
+    return (
+        <BrowserRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to={`/`}>go to home</Link>
+                    </li>
+                    <li>
+                        <Link to={`/test`}>go to test</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<p>home</p>}></Route>
+                <Route path="/test" element={<p>test</p>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+export default Navbar
