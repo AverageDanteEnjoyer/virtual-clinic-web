e450a125da6ec079848f61386728cfbdfa28f24d
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2022-11-16 16:23:16 +0100

    remove redundant includes, fix indent, add jetbrains configuration files to gitignore

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,31 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
 } from "react-router-dom";
 
-const Navbar:React.FC=(props:any) => {
-    return (
-        <BrowserRouter>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={`/`}>go to home</Link>
-                    </li>
-                    <li>
-                        <Link to={`/test`}>go to test</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Routes>
-                <Route path="/" element={<p>home</p>}></Route>
-                <Route path="/test" element={<p>test</p>}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+const Navbar: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to={`/`}>go to home</Link>
+          </li>
+          <li>
+            <Link to={`/test`}>go to test</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<p>home</p>}></Route>
+        <Route path="/test" element={<p>test</p>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default Navbar
