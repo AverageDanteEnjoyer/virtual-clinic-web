f41a9b39945266d12127b305b09cc451b07710a7
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2022-11-21 20:47:46 +0100

    Reformat previously added code with prettier

--- src/components/StyledSelect.tsx
+++ src/components/StyledSelect.tsx
@@ -1,44 +1,37 @@
 import React from 'react';
-import { Select, SelectProps} from 'antd';
+import { Select, SelectProps } from 'antd';
 
-interface StyledSelectProps extends SelectProps{
-  customOptions?:{label:string, children:{label:string, value:string, disabled?:boolean, className?:string}[]}[],
+interface StyledSelectProps extends SelectProps {
+  customOptions?: {
+    label: string;
+    children: { label: string; value: string; disabled?: boolean; className?: string }[];
+  }[];
 }
 
-const StyledSelect: React.FC<StyledSelectProps> = ({customOptions, defaultValue, mode, style}:StyledSelectProps) => {
-  const {Option, OptGroup} = Select;
+const StyledSelect: React.FC<StyledSelectProps> = ({ customOptions, defaultValue, mode, style }: StyledSelectProps) => {
+  const { Option, OptGroup } = Select;
   const groups = customOptions?.map((group) => {
     const options = group.children.map((option) => (
-      <Option className={option.className}
-        key={option.value}
-        value={option.value}
-      >
+      <Option className={option.className} key={option.value} value={option.value}>
         {option.label}
       </Option>
-    ))
+    ));
     return (
-      <OptGroup
-        key={group.label}
-        label={group.label}
-      >
+      <OptGroup key={group.label} label={group.label}>
         {options}
       </OptGroup>
     );
-  })
+  });
   return (
-    <Select
-      mode={mode}
-      defaultValue={defaultValue}
-      style={style}
-    >
+    <Select mode={mode} defaultValue={defaultValue} style={style}>
       {groups}
     </Select>
   );
-}
+};
 
-StyledSelect.defaultProps={
-  customOptions:[],
-  style:{width:"30%"},
-}
+StyledSelect.defaultProps = {
+  customOptions: [],
+  style: { width: '30%' },
+};
 
 export default StyledSelect;
