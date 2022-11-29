cdec1a20000aa9968189573dd3e0bf04e05cc9b2
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2022-11-29 13:14:58 +0100

    use destruct while mapping groups and options in Select component

--- src/Components/Select/index.tsx
+++ src/Components/Select/index.tsx
@@ -18,14 +18,14 @@ const StyledSelect = ({
   placeholder,
 }: StyledSelectProps) => {
   const { Option, OptGroup } = Select;
-  const groups = customOptions?.map((group) => {
-    const options = group.children.map((option) => (
-      <Option className={option.className} key={option.value} value={option.value}>
-        {option.label}
+  const groups = customOptions?.map(({ children, label }) => {
+    const options = children.map(({ className, value, label }) => (
+      <Option className={className} key={value} value={value}>
+        {label}
       </Option>
     ));
     return (
-      <OptGroup key={group.label} label={group.label}>
+      <OptGroup key={label} label={label}>
         {options}
       </OptGroup>
     );
