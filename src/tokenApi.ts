bd20ad9066b6ddcb27d30e5d582cc0dc359217c8
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2022-12-06 12:18:09 +0100

    convert function in tokenApi.ts to arrow functions

--- src/tokenApi.ts
+++ src/tokenApi.ts
@@ -1,12 +1,12 @@
-export function getToken() {
+export const getToken = () => {
   const token = localStorage.getItem('token');
   return token ? JSON.parse(token).token : null;
-}
+};
 
-export function setToken(token: string | null) {
+export const setToken = (token: string | null) => {
   localStorage.setItem('token', JSON.stringify({ token: token }));
-}
+};
 
-export function removeToken() {
+export const removeToken = () => {
   localStorage.removeItem('token');
-}
+};
