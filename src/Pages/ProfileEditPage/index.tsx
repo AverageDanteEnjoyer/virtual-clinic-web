2cb27a73061b2eb018855d6c7c2c8e88f0b7a2fe
Blindeen <kuba.szmajda@gmail.com>
2022-12-13 08:01:28 +0100

    add headline on the profile edit page

--- src/Pages/ProfileEditPage/index.tsx
+++ src/Pages/ProfileEditPage/index.tsx
@@ -1,10 +1,12 @@
 import ProfileEditForm from '../../Containers/ProfileEditForm';
 import Navbar from '../../Components/Navbar';
+import { StyledTitle } from '../../Components/Typography/styles';
 
 const ProfileEditPage = () => {
   return (
     <>
       <Navbar />
+      <StyledTitle>Profile edit</StyledTitle>
       <ProfileEditForm />
     </>
   );
