1c5d3bd73141a26b1db0884a9530c5e8bbdbcc08
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2022-12-13 14:28:04 +0100

    add lodash package

--- src/Pages/DoctorEditProfilePage/index.tsx
+++ src/Pages/DoctorEditProfilePage/index.tsx
@@ -1,12 +1,10 @@
 import Navbar from '../../Components/Navbar';
 import ProfessionSelector from '../../Containers/ProfessionSelector';
-import EditProfileForm from '../../Containers/EditProfileForm';
 
 const DoctorEditProfilePage = () => {
   return (
     <>
       <Navbar />
-      <EditProfileForm />
       <ProfessionSelector />
     </>
   );
