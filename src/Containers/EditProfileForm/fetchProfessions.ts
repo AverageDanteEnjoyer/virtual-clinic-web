2090729d74c0c9925e3a1c5407b39b1c5c095280
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2022-12-19 16:04:09 +0100

    refactor fetchProfessions.ts, replacing then() with await

--- src/Containers/EditProfileForm/fetchProfessions.ts
+++ src/Containers/EditProfileForm/fetchProfessions.ts
@@ -6,18 +6,18 @@ export const fetchAllProfessions = async ({ name, perPage, pageIndex }: searchPa
   const token = getLocalStorageResource('token');
   if (!token) return;
 
-  return fetch(`${API_URL}/api/v1/professions/?name=${name}&per_page=${perPage}&page=${pageIndex}`, {
+  const response = await fetch(`${API_URL}/api/v1/professions/?name=${name}&per_page=${perPage}&page=${pageIndex}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       Authorization: token,
     },
-  })
-    .then((response) => response.json())
-    .then((responseBody) => ({
-      options: responseBody.data.map((value: { key: number; name: string }) => value.name),
-      total: responseBody.total,
-    }));
+  });
+  const responseBody = await response.json();
+  return {
+    options: responseBody.data.map((value: { key: number; name: string }) => value.name),
+    total: responseBody.total,
+  };
 };
 
 export const fetchDoctorProfessions = async () => {
@@ -25,32 +25,32 @@ export const fetchDoctorProfessions = async () => {
   if (!token) return;
   const { userID } = getDataFromToken();
 
-  return await fetch(`${API_URL}/api/v1/doctors/${userID}/professions/`, {
+  const response = await fetch(`${API_URL}/api/v1/doctors/${userID}/professions/`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       Authorization: token,
     },
-  })
-    .then((response) => response.json())
-    .then((responseBody) => responseBody.data.map((value: { key: number; name: string }) => value.name));
+  });
+  const responseBody = await response.json();
+  return responseBody.data.map((value: { key: number; name: string }) => value.name);
 };
 
 export const createNewProfession = async (option: string) => {
   const token = getLocalStorageResource('token');
   if (!token) return { success: false, message: 'Token expired' };
 
-  return fetch(`${API_URL}/api/v1/professions/`, {
+  const response = await fetch(`${API_URL}/api/v1/professions/`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       Authorization: token,
     },
     body: JSON.stringify({ profession: { name: option } }),
-  })
-    .then((response) => response.json())
-    .then((responseBody) => ({
-      success: Boolean(responseBody.data),
-      message: responseBody.errors ? responseBody.errors.name : '',
-    }));
+  });
+  const responseBody = await response.json();
+  return {
+    success: Boolean(responseBody.data),
+    message: responseBody.errors ? responseBody.errors.name : '',
+  };
 };
