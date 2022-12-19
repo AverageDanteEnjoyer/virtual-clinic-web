93fb1bca3ef6e46a09c554f4f2b100ec69f53964
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2022-12-19 17:04:58 +0100

    add method POST

--- src/Containers/DoctorManageProcedures/index.tsx
+++ src/Containers/DoctorManageProcedures/index.tsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from 'react';
-import { API_URL } from '../../api';
+import { useState, useEffect, FormEvent } from 'react';
+import { Col, Form, FormItemProps, Row } from 'antd';
 import { getDataFromToken, getLocalStorageResource } from '../../localStorageAPI';
+import Spin from '../../Components/Spin';
+import { API_URL } from '../../api';
+import './index.css';
 
-import { Divider, Pagination, Select } from 'antd';
-import CustomSelect from '../../Components/Select';
-import Button from '../../Components/Button';
+export interface formItem extends FormItemProps {
+  type: string;
+}
 
 type doctorProcedures = {
   id: number;
@@ -12,15 +15,55 @@ type doctorProcedures = {
   needed_time_min: number;
 };
 
+// type procedureInfo = {
+//   name: string;
+//   needed_time_min: number;
+// };
+
 const DoctorManageProcedures = () => {
-  const [actualPage, setActualPage] = useState<number>(1);
-  const [totalPages, setTotalPages] = useState<number>(0);
-  const [perPage, setPerPage] = useState<number>(10);
-  const [searchInput, setSearchInput] = useState<string>('');
-
+  // const [actualPage, setActualPage] = useState<number>(1);
+  // const [totalPages, setTotalPages] = useState<number>(0);
+  // const [perPage, setPerPage] = useState<number>(10);
+  // const [searchInput, setSearchInput] = useState<string>('');
   const [doctorProcedures, setDoctorProcedures] = useState<doctorProcedures[]>([]);
+  const [procedureName, setProcedureName] = useState<string>('');
+  const [neededTime, setNeededTime] = useState<string | number>('');
 
-  //load current doctor procedures
+  const handleChangeNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNeededTime(+event.target.value);
+  };
+
+  const [loading, setLoading] = useState(false);
+  const [alerts, setAlerts] = useState<
+    {
+      type: 'success' | 'warning' | 'error' | 'info';
+      message: string;
+      description?: string;
+    }[]
+  >([]);
+
+  const handleSumbit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const token = getLocalStorageResource('token');
+    if (!token) return;
+
+    await fetch(`${API_URL}/api/v1/procedures`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        procedure: {
+          name: procedureName,
+          needed_time_min: neededTime,
+        },
+      }),
+    }).then(function (response) {
+      console.log(response);
+    });
+  };
+
   useEffect(() => {
     const getDoctorProcedures = async () => {
       const token = getLocalStorageResource('token');
@@ -45,9 +88,35 @@ const DoctorManageProcedures = () => {
   }, []);
 
   return (
-    <div>
-      <h1>procedury</h1>
-    </div>
+    <Spin spinning={loading} tip="waiting for server response...">
+      <div className="center">
+        <form className="wrap" onSubmit={handleSumbit}>
+          <fieldset className="field-area">
+            <label htmlFor="name">Procedure name:</label>
+            <input
+              type="text"
+              id="name"
+              required
+              placeholder="Input your new procedure"
+              value={procedureName}
+              onChange={(e) => setProcedureName(e.target.value)}
+            />
+          </fieldset>
+          <fieldset className="field-area">
+            <label htmlFor="num">Needed time:</label>
+            <input
+              type="number"
+              id="number"
+              required
+              value={neededTime}
+              onChange={handleChangeNumber}
+              className="inp-num"
+            />
+          </fieldset>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    </Spin>
   );
 };
 
