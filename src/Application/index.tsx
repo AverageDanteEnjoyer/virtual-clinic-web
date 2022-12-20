db9f0873f4a0d7a8dc95cf6a0e548f08cbe7b486
Blindeen <kuba.szmajda@gmail.com>
2022-12-20 10:58:20 +0100

    create separate file with mapped private routes and change name to mappedPrivateRoutes

--- src/Application/index.tsx
+++ src/Application/index.tsx
@@ -4,22 +4,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import routes from '../routes';
 import { clearLocalStorage, getAccountType, getDataFromToken } from '../localStorageAPI';
 import { SessionInfoContext } from '../SessionInfoContext';
-import { PrivateRoute, equals, notEquals } from '../privateRoute';
 
-import { userType } from '../SessionInfoContext';
 import ComponentsPage from '../Pages/ComponentsPage';
-import RegistrationPage from '../Pages/RegistrationPage';
-import LoginPage from '../Pages/LoginPage';
 import HomePage from '../Pages/HomePage';
 import AuthVerify from '../AuthVerify';
-import ProfileEditPage from '../Pages/ProfileEditPage';
-
-interface privateRouteItem {
-  path: string;
-  children: JSX.Element;
-  redirectPath: string;
-  condition: () => boolean;
-}
+import { mappedPrivateRoutes } from '../mappedPrivateRoutes';
 
 const Application = () => {
   const { setAccountType } = useContext(SessionInfoContext);
@@ -35,44 +24,12 @@ const Application = () => {
     }
   }, []);
 
-  const privateRouteItems: privateRouteItem[] = [
-    {
-      path: routes.logIn,
-      children: <LoginPage />,
-      redirectPath: routes.home,
-      condition: () => equals(userType.GUEST),
-    },
-    {
-      path: routes.register,
-      children: <RegistrationPage />,
-      redirectPath: routes.home,
-      condition: () => equals(userType.GUEST),
-    },
-    {
-      path: routes.editProfile,
-      children: <ProfileEditPage />,
-      redirectPath: routes.logIn,
-      condition: () => notEquals(userType.GUEST),
-    },
-  ];
-
-  const privateRouteItemsJSX = privateRouteItems.map(({ path, children, redirectPath, condition }) => (
-    <Route
-      path={path}
-      element={
-        <PrivateRoute redirectPath={redirectPath} condition={condition}>
-          {children}
-        </PrivateRoute>
-      }
-    />
-  ));
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path={routes.home} element={<HomePage />} />
         <Route path={routes.components} element={<ComponentsPage />} />
-        {privateRouteItemsJSX}
+        {mappedPrivateRoutes}
       </Routes>
       <AuthVerify />
     </BrowserRouter>
