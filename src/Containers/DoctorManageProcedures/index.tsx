fbe6a02a014968e4e5a7b08295d522f31ad4b0fd
Jakub Kozlowski <jakub.kozlowski1104@gmail.com>
2022-12-20 23:23:42 +0100

    add paginated table

--- src/Containers/DoctorManageProcedures/index.tsx
+++ src/Containers/DoctorManageProcedures/index.tsx
@@ -1,46 +1,47 @@
-import { useState, useEffect, FormEvent } from 'react';
-import { Col, Form, FormItemProps, Row } from 'antd';
+import './index.css';
+import { API_URL } from '../../api';
 import { getDataFromToken, getLocalStorageResource } from '../../localStorageAPI';
 import Spin from '../../Components/Spin';
-import { API_URL } from '../../api';
-import './index.css';
+import { useState, useEffect, FormEvent } from 'react';
+import { Col, Form, FormItemProps, Row, Table } from 'antd';
 
-export interface formItem extends FormItemProps {
-  type: string;
-}
-
-type doctorProcedures = {
+type doctorProceduresType = {
   id: number;
   name: string;
   needed_time_min: number;
 };
 
-// type procedureInfo = {
-//   name: string;
-//   needed_time_min: number;
-// };
-
 const DoctorManageProcedures = () => {
-  // const [actualPage, setActualPage] = useState<number>(1);
-  // const [totalPages, setTotalPages] = useState<number>(0);
-  // const [perPage, setPerPage] = useState<number>(10);
-  // const [searchInput, setSearchInput] = useState<string>('');
-  const [doctorProcedures, setDoctorProcedures] = useState<doctorProcedures[]>([]);
+  const [actualPage, setActualPage] = useState<number>(1);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [doctorProcedures, setDoctorProcedures] = useState<doctorProceduresType[]>([]);
   const [procedureName, setProcedureName] = useState<string>('');
   const [neededTime, setNeededTime] = useState<string | number>('');
+  const [loading, setLoading] = useState(false);
 
   const handleChangeNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNeededTime(+event.target.value);
   };
 
-  const [loading, setLoading] = useState(false);
-  const [alerts, setAlerts] = useState<
-    {
-      type: 'success' | 'warning' | 'error' | 'info';
-      message: string;
-      description?: string;
-    }[]
-  >([]);
+  const getDoctorProcedures = async () => {
+    setLoading(true);
+    const token = getLocalStorageResource('token');
+    if (!token) return;
+    const { userID } = getDataFromToken();
+    const response = await fetch(`${API_URL}/api/v1/doctors/${userID}/procedures/?per_page=${100}&page=${1}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    });
+    const responseDetails = await response.json();
+    console.log(responseDetails);
+    setLoading(false);
+    setDoctorProcedures(responseDetails.data);
+    setTotalPages(responseDetails.data.length);
+    console.log(responseDetails.data);
+  };
 
   const handleSumbit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -62,33 +63,39 @@ const DoctorManageProcedures = () => {
     }).then(function (response) {
       console.log(response);
     });
+    console.log('wywołaj get');
+    getDoctorProcedures();
   };
 
   useEffect(() => {
-    const getDoctorProcedures = async () => {
-      const token = getLocalStorageResource('token');
-      if (!token) return;
-      const { userID } = getDataFromToken();
-      const response = await fetch(
-        `${API_URL}/api/v1/doctors/${userID}/procedures/?name=${searchInput}&per_page=${perPage}&page=${actualPage}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: token,
-          },
-        }
-      );
-      const responseDetails = await response.json();
-      console.log(responseDetails);
-      setDoctorProcedures(responseDetails.data);
-    };
-
     getDoctorProcedures();
   }, []);
 
+  const columns = [
+    {
+      title: 'Procedure ID',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: 'Procedure name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Procedure time(min)',
+      dataIndex: 'needed_time_min',
+      key: 'needed_time_min',
+    },
+    {
+      title: 'delete',
+      dataIndex: 'address',
+      key: 'address',
+    },
+  ];
+
   return (
-    <Spin spinning={loading} tip="waiting for server response...">
+    <>
       <div className="center">
         <form className="wrap" onSubmit={handleSumbit}>
           <fieldset className="field-area">
@@ -116,7 +123,22 @@ const DoctorManageProcedures = () => {
           <button type="submit">Add</button>
         </form>
       </div>
-    </Spin>
+      <Spin spinning={loading} tip="waiting for server response...">
+        <Table
+          loading={loading}
+          columns={columns}
+          dataSource={doctorProcedures}
+          pagination={{
+            pageSize: 5,
+            total: totalPages,
+            onChange: (page) => {
+              console.log(page);
+              setActualPage(page);
+            },
+          }}
+        ></Table>
+      </Spin>
+    </>
   );
 };
 
