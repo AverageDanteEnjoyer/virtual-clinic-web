1ea2ea4de92ca004f1df1f99d3414da521fc1903
Damian Mendel <mendeldamian01@gmail.com>
2023-01-12 16:29:31 +0100

    remove styles variable

--- src/Components/PaginatedTable/styles.ts
+++ src/Components/PaginatedTable/styles.ts
@@ -1,17 +1,13 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { Input as InputAntd } from 'antd';
-
-const styles = css`
-  border-radius: 20px;
-  font-family: CircularStd, sans-serif;
-`;
 
 export const FilterDropdown = styled.div`
   padding: 8px;
 `;
 
 export const Input = styled(InputAntd)`
-  ${styles};
+  border-radius: 20px;
+  font-family: CircularStd, sans-serif;
   width: 200px;
   display: block;
   font-size: 14px;
