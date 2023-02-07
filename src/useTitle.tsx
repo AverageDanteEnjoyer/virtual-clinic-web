c7cec6cf080a0d04c7ac6f936373ff874e05928c
Damian Mendel <mendeldamian01@gmail.com>
2023-02-07 10:26:25 +0100

    add optional parameter

--- src/useTitle.tsx
+++ src/useTitle.tsx
@@ -4,13 +4,13 @@ import { useLocation } from 'react-router-dom';
 import routes from './routes';
 import { Store } from './store';
 
-const useTitle = () => {
+const useTitle = (title?: string) => {
   const { dispatch } = useContext(Store);
   const location = useLocation();
 
   useEffect(() => {
     const route = Object.values(routes).find((route) => route.path === location.pathname);
-    dispatch({ type: 'setTitle', payload: { title: route?.title || 'Virtual Clinic' } });
+    dispatch({ type: 'setTitle', payload: { title: title || route?.title || 'Virtual Clinic' } });
   }, [location, dispatch]);
 };
 
