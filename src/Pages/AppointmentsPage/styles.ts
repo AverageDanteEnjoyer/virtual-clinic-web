b5c0adc1f672394295c627fe1e280940b8a6e605
Blindeen <kuba.szmajda@gmail.com>
2023-02-20 15:50:04 +0100

    modify padding

--- src/Pages/AppointmentsPage/styles.ts
+++ src/Pages/AppointmentsPage/styles.ts
@@ -6,9 +6,5 @@ import palette from 'palette';
 export const PanelCol = styled(Col)`
   background-color: ${palette.whiteSmoke};
   border-radius: 10px;
-  padding-bottom: 50px;
-
-  &:last-of-type {
-    margin-bottom: 50px;
-  }
+  padding-bottom: 40px;
 `;
