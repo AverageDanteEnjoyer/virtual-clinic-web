5ed7230b417e0a72f9cbd834ddbbc61f5703524b
Blindeen <kuba.szmajda@gmail.com>
2023-02-22 17:13:06 +0100

    remove redundant prop

--- src/Components/Carousel/index.tsx
+++ src/Components/Carousel/index.tsx
@@ -21,7 +21,7 @@ const Carousel = ({ autoplay = true, effect = 'fade', dotPosition, dots, easing 
   return (
     <StyledCarousel autoplay={autoplay} dotPosition={dotPosition} dots={dots} easing={easing} effect={effect}>
       {CarouselData.map((img, idx) => {
-        return <Image key={idx} src={img} onClick={undefined} />;
+        return <Image key={idx} src={img} />;
       })}
     </StyledCarousel>
   );
